556b2cb00cd2e0a38b389e4063ecb535ca3d0394
Lammax <lammax@mail.ru>
2020-07-24 13:03:36 +0300

    V 1.0.1 Basic dummy classes

--- Sources/AC_iOS_SDK/AC_iOS_SDK.swift
+++ Sources/AC_iOS_SDK/AC_iOS_SDK.swift
@@ -1,4 +1,37 @@
+import Foundation
+import CoreLocation
+
 struct AC_iOS_SDK {
     var text = "Hello, World!"
-    var version = "1.0.0"
+    var version = "1.0.1"
+}
+
+class AugmentedCity {
+    
+    class Common {
+        static func info() {
+            let sdk = AC_iOS_SDK()
+            
+            print("AC_iOS_SDK, text = \(sdk.text), version = \(sdk.version)")
+        }
+    }
+    
+    class Localization {
+        static func checkNearPlaceholders() {
+            print("checkNearPlaceholders")
+        }
+
+        static func checkNearCity() {
+            print("checkNearCity")
+        }
+    }
+    
+    class Objects {
+        
+        static func addObject(type: String) {
+            print("Added object: \(type)")
+        }
+        
+    }
+    
 }
