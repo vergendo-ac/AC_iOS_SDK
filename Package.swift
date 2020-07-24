ec73dec9926ba0605df14240b7c25c9b5fc34589
Lammax <lammax@mail.ru>
2020-07-24 13:11:17 +0300

    Target test

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AC_iOS_SDK",
-            targets: ["AC_iOS_SDK"]),
+            targets: ["AC_iOS_SDK", "myplace_admin_ios"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
