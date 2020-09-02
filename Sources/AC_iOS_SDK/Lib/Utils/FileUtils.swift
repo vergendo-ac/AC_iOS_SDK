ab64eeb122b9976d138f26550cdc2aa7bea86a8a
Lammax <lammax@mail.ru>
2020-09-02 19:06:02 +0300

    Net SDK v.1.0.50

--- Sources/AC_iOS_SDK/Lib/Utils/FileUtils.swift
+++ Sources/AC_iOS_SDK/Lib/Utils/FileUtils.swift
@@ -13,12 +13,15 @@ open class FileUtils {
     public class func writeFile(name: String, data: Data, fileExtension: String = "png") -> URL? {
         let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         
-        let fileURL = URL(fileURLWithPath: name, relativeTo: directoryURL).appendingPathExtension(fileExtension)
+        var fileURL = URL(string: directoryURL.absoluteString + "/" + name + "." + fileExtension)
+        if #available(OSX 10.11, *) {
+            fileURL = URL(fileURLWithPath: name, relativeTo: directoryURL).appendingPathExtension(fileExtension)
+        }
         
         // Save the data
         do {
-         try data.write(to: fileURL)
-            print("File saved: \(fileURL.absoluteURL)")
+         try data.write(to: fileURL!)
+            print("File saved: \(fileURL!.absoluteURL)")
             return fileURL
         } catch {
          // Catch any errors
