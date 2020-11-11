f594f0bba571516f60ef3dc1b7bd387ca8ec6f95
Lammax <lammax@mail.ru>
2020-11-11 15:46:44 +0300

    Net SDK update

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name:"AC_iOS_AR", url: "https://github.com/vergendo-ac/AC_iOS_AR.git", from: "2.12.4"),
-        .package(name:"AC_iOS_NET", url: "https://github.com/vergendo-ac/AC_iOS_NET.git", from: "2.12.17")
+        .package(name:"AC_iOS_NET", url: "https://github.com/vergendo-ac/AC_iOS_NET.git", from: "2.12.18")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
