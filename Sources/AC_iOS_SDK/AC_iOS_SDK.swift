0ed64f946bb5208eb1345ce56451726d94bc08e9
Lammax <lammax@mail.ru>
2020-12-11 15:58:36 +0300

    Remove takeARPhoto func

--- Sources/AC_iOS_SDK/AC_iOS_SDK.swift
+++ Sources/AC_iOS_SDK/AC_iOS_SDK.swift
@@ -156,10 +156,6 @@ open class SDK {
             ARHelper.stopAR()
         }
         
-        public static func takePhoto(completion: @escaping (Data?, NSError?, UIDeviceOrientation?) -> Void) {
-            ARHelper.takePhoto(completion: completion)
-        }
-        
         public static func delete(by stickerID: Int) {
             ARHelper.delete(by: stickerID)
         }
