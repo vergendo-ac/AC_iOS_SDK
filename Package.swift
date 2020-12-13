fb8fdc78f052394998105bc3daaf432612d3ced3
Lammax <lammax@mail.ru>
2020-12-13 16:04:11 +0300

    AR SDK video node type fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["AC_iOS_SDK"]),
     ],
     dependencies: [
-        .package(name:"AC_iOS_AR", url: "https://github.com/vergendo-ac/AC_iOS_AR.git", from: "2.12.18"),
+        .package(name:"AC_iOS_AR", url: "https://github.com/vergendo-ac/AC_iOS_AR.git", from: "2.12.19"),
         .package(name:"AC_iOS_NET", url: "https://github.com/vergendo-ac/AC_iOS_NET.git", from: "2.12.18")
     ],
     targets: [
